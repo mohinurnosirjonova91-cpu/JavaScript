8e7b6a79206f8f27d5e1eb35522de515d3c19134
Mohinur Nosirjonova <mohinurnosirjonova91@gmail.com>
2026-02-28 13:16:46 +0500

    done

---
+++ FullStack qoshimcha/Vazifalar.js
@@ -0,0 +1,56 @@
+//// Homework task
+
+////1-vazifa: Siz ism va tugulgan yildan maxfiy kod yasab berishingiz kerak.
+
+// const name="Asadbek";
+// const birthYear=2005;
+//
+// //Ismdagi 1-harfni olish.
+// const firstLetter=name[0];
+//
+// //Qolgan qismi
+// const restOFName=name.slice(1);
+//
+// //Tugulgan yilni srtingga aylantirish.
+// const yearString = String(birthYear);
+//
+// //Birlashtirish
+// const secretCode = firstLetter + yearString+ restOFName;
+//
+// console.log("Maxfiy kod: "+ secretCode);
+
+
+////2-vazifa: Toshkentdagi hayvonot bogiga necha yoshli insonlar kirishini tekshirish kerak.
+
+// const age = 10;
+// const hasTicket = true;
+// const isVip = false;
+//
+// console.log(age > 12 && hasTicket) || isVip;
+
+
+////3-vazifa: Online dokon uchun savatcha qilyapsiz. Xaridorga chegirma berish yoki bermaslikni mantiqiy aniqlang.
+
+// const totalSum = 50; // Xaridning umumiy summasi
+// const isPremiumMember = false; // Mijoz premiummi yoki yo'q
+// const hasPromoCode = true; // Mijozda promo-kod borligi
+//
+// const getDiscount=( totalSum > 100 && isPremiumMember ) || hasPromoCode;
+//
+// console.log(getDiscount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
