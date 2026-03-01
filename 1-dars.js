1c44a64231eb8f23eebc371f2f6fb1815b3d4be8
Mohinur Nosirjonova <mohinurnosirjonova91@gmail.com>
2026-03-01 13:19:22 +0500

    Recovered code

---
+++ 1-dars.js
@@ -0,0 +1,156 @@
+// LESSON 2
+// alert('Hello world!');
+
+// let name = "Mohinur"; //string
+// let surname = "Noairjonova";
+// let age = 18
+// console.log(name)
+// console.log(surname)
+// console.log(age)
+// console.log(age)
+
+
+
+
+
+
+// lkmlkl
+// name = "Oyzoda"
+// console.log(name)
+
+// const id = "AA0"
+// console.log(id)
+
+// let age = 20; //number
+// let price = 19.99;
+// console.log(age)
+// console.log(price)
+
+// null - bo'sh qiymat
+
+// symbol - unique
+
+// let id = Symbol('id');
+// console.log(id)
+
+
+// LESSON 3
+// 'use strict';
+// firstName = "Mohinur";
+// console.log(firstName)
+
+// // LESSON 4
+// ARIFMETIK OPERATORLAR
+
+// let a = 5, b = 10;
+// console.log(a + b);  //15
+// console.log(a - b);  //-5
+// console.log(a * b);  //50
+// console.log(a / b);  //0.5
+
+// console.log(a % b);  //5
+// console.log(a++);    //5
+// console.log(a);     //6
+
+// console.log(++a);    //6 (7)
+// console.log(a);     //
+
+// SOLISHTIRISH OPERATORLAR
+// let a = 5, b = 10, c = '5';
+// const z = a;
+
+// console.log(a == c); //true // qiymatga qarab solishtiradi
+// console.log(a === c); //false // turiga qarab solishtiradi
+// console.log (a == z); //true
+// console.log (a === z); //false
+// console.log(a != b); //true
+// console.log(a != z); //false
+// console.log(a > b); //false
+// console.log(a < b); //true
+// console.log(a >= b); //false
+// console.log(a <= b); //true
+
+
+// TAYINLASH OPERATORLARI
+
+// let a = 5, b = 10, c = 15;
+
+// a = b; //10
+// console.log(a);
+
+// a += b; //20
+// //a = a + b
+// console.log(a);
+
+// a -= b; //10
+// console.log(a);
+
+// c *= b; //150
+// console.log(c);
+
+// c /= 5; //30
+// c = c / b
+// console.log(c);
+
+// b %= 2; //0
+// b = b % 2
+// console.log(b);
+
+//VAZIFA
+// 2-vazifa //Universitet baxo dasturi
+
+// let mathGrade =5;
+// let physicsGrade =4;
+// let historyGrade =5;
+//
+// ortachaQiymat = (mathGrade + physicsGrade + historyGrade) / 3; // ortacha baxoni baxolash.
+//
+// let total=0;
+//
+// total += mathGrade;
+// total += physicsGrade;
+// total += historyGrade;
+//
+// total /= 3;
+// console.log(total);
+//
+// console.log(typeof mathGrade); // typeni bilish.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
